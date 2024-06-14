60b9c089ecbd92211ac8c432f2f3184b89d607eb
lin <ichenzhl@qq.com>
2024-06-14 12:03:12 +0800

    开发ing

--- apps/home/internal/iface2/imail.go
+++ apps/home/internal/iface2/imail.go
@@ -1,1 +1,12 @@
 package iface2
+
+import "grain_game/proto/gen/ret"
+
+type IMail interface {
+	IPlayerModule
+	SendMail()
+	ReadMail(mailId uint64)
+	DeleteMail(mailId uint64)
+	GetMailReward(mailId uint64) ret.Code
+	GetMails(mailIds []uint64)
+}
