6624029e210979114d0d9619cc6098b7047eee5a
lin <ichenzhl@qq.com>
2024-06-14 11:57:40 +0800

    开发ing

--- apps/home/internal/player_actor.go
+++ apps/home/internal/player_actor.go
@@ -1,12 +1,10 @@
 package internal
 
 import (
-	"fmt"
 	"github.com/chenxyzl/grain/actor"
 	"github.com/chenxyzl/grain/utils/helper"
 	"grain_game/apps/common1"
 	"grain_game/apps/home/internal/iface2"
-	"grain_game/apps/shared1/helper1"
 	"grain_game/apps/shared1/iface1"
 	pbi "grain_game/proto/gen/inner"
 	"grain_game/proto/gen/ret"
@@ -18,26 +16,22 @@ var _ actor.IActor = (*Player)(nil)
 type Player struct {
 	*iface1.BaseEntity
 	modules    map[string]iface2.IPlayerModule
-	modulesSl  []IPlayerModuleLife //for range
+	modulesSl  []iface2.IPlayerModule //for range
 	cancelTick actor.CancelScheduleFunc
 }
 
 func NewPlayer() *Player {
-	return &Player{BaseEntity: iface1.NewBaseEntity(), modules: make(map[string]IPlayerModuleLife)}
+	return &Player{BaseEntity: iface1.NewBaseEntity(), modules: make(map[string]iface2.IPlayerModule)}
 }
 
 func (p *Player) Started() {
 	//todo load db?
 	//
-	for idx, f := range p.modulesSl {
-		if cod := f.OnInit(); cod != ret.Code_Ok {
-			helper1.MustOk(cod, "OnInit.failed", fmt.Sprintf("idx:%v", idx))
-		}
+	for _, f := range p.modulesSl {
+		f.OnInit()
 	}
-	for idx, f := range p.modulesSl {
-		if cod := f.OnStarted(); cod != ret.Code_Ok {
-			helper1.MustOk(cod, "OnStarted.failed", fmt.Sprintf("idx:%v", idx))
-		}
+	for _, f := range p.modulesSl {
+		f.OnStarted()
 	}
 	//
 	p.cancelTick = p.ScheduleSelfRepeated(time.Second, time.Second, &pbi.Tick{})
@@ -48,10 +42,8 @@ func (p *Player) PreStop() {
 		p.cancelTick()
 		p.cancelTick = nil
 	}
-	for idx, f := range p.modulesSl {
-		if cod := f.OnPreStop(); cod != ret.Code_Ok {
-			helper1.MustOk(cod, "OnPreStop.failed", fmt.Sprintf("idx:%v", idx))
-		}
+	for _, f := range p.modulesSl {
+		f.OnPreStop()
 	}
 }
 
