1cbc9cc9ac95d4f1ee28d2c05786786ae87b7100
lin <ichenzhl@qq.com>
2024-07-08 15:37:10 +0800

    开发ing add table [add] 增加table的解析

--- apps/shared/table/1rowloader.go
+++ apps/shared/table/1rowloader.go
@@ -4,12 +4,15 @@ import (
 	"errors"
 	"fmt"
 	"github.com/xuri/excelize/v2"
+	"reflect"
 	"strings"
 )
 
 type rowLoader[k comparable, T any] struct {
-	list []T
-	data map[k]T
+	list        []T
+	data        map[k]T
+	idxes       map[string]int
+	defaultVals map[string]string
 }
 
 // load  unmarshal from csv
@@ -38,7 +41,25 @@ func (l *rowLoader[K, T]) load(file string) error {
 		return errors.Join(fmt.Errorf("get rows err, excel:%v", excelFile), err)
 	}
 
-	for _, row := range rows {
+	for rowIdx, row := range rows {
+		_ = rowIdx
+		switch rowIdx {
+		case 0:
+			l.parseHeader(row)
+		case 1: //name, ignore
+		case 2: //default
+			l.parseDefault(row)
+		case 3: //link
+			l.parseLink(row)
+		case 4: //param type, ignore
+		case 5: //export type, ignore
+		default:
+			l.parseData(row)
+		}
+		for _, val := range row {
+			fmt.Println(val)
+			//
+		}
 		//todo parse row
 	}
 	return err
@@ -64,4 +85,34 @@ func (l *rowLoader[K, T]) Range(f func(k K, v T) bool) {
 	for k, v := range l.data {
 		f(k, v)
 	}
+}
+
+func (l *rowLoader[K, T]) parseHeader(row []string) {
+	l.idxes = make(map[string]int)
+	var t T
+	tt := reflect.TypeOf(t)
+Next:
+	for i := range tt.NumField() {
+		field := tt.Field(i)
+		tag := field.Tag.Get("json")
+		for idx, name := range row {
+			if name == tag {
+				l.idxes[tag] = idx
+				break Next
+			}
+		}
+		panic(fmt.Errorf("not found index, jsosn tag name: %s", tag))
+	}
+}
+func (l *rowLoader[K, T]) parseDefault(row []string) {
+	l.defaultVals = make(map[string]string)
+	for name, idx := range l.idxes {
+		l.defaultVals[name] = row[idx]
+	}
+}
+func (l *rowLoader[K, T]) parseLink(row []string) {
+	//先解析link?
+}
+func (l *rowLoader[K, T]) parseData(row []string) {
+
 }
