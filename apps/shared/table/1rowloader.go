65cb033b5d06d10c42d0e61f1dc50cd7e7792c6c
lin <ichenzhl@qq.com>
2024-07-10 11:55:23 +0800

    开发ing add table [new] 设计表, 修改定义"_模板.xlsx" 1

--- apps/shared/table/1rowloader.go
+++ apps/shared/table/1rowloader.go
@@ -8,37 +8,49 @@ import (
 	"strings"
 )
 
+const subTag = "-"
+const nestTag = "+"
+const aliseTag = "="
+
 type rowLoader[k comparable, T any] struct {
-	list        []T
-	data        map[k]T
-	idxes       map[string]int
-	defaultVals map[string]string
+	//数据相关
+	list []T     //数据 list
+	data map[k]T //数据的key/val
+	//解析相关~
+	fileFs      *excelize.File            //文件句柄 用于同表的lin和别名
+	excelName   string                    //excel名字
+	sheetName   string                    //sheet名字
+	idxes       map[string]int            //名字对应的索引
+	defaultVals map[int]string            //默认值
+	aliseName   map[int]map[string]string //别名
+	link        map[int][][]string        //子表
 }
 
 // load  unmarshal from csv
 func (l *rowLoader[K, T]) load(file string) error {
 	var err error
-	var excelFile = file
-	var sheetName = "Sheet1"
+	l.excelName = file
+	l.sheetName = "Sheet1"
 	var fileAndSheetName = strings.Split(file, ";")
 	if len(fileAndSheetName) > 1 {
-		excelFile = fileAndSheetName[0]
-		sheetName = fileAndSheetName[1]
+		l.excelName = fileAndSheetName[0]
+		l.sheetName = fileAndSheetName[1]
 	}
 
-	f, err := excelize.OpenFile(excelFile)
+	f, err := excelize.OpenFile(l.excelName)
 	if err != nil {
 		return errors.Join(fmt.Errorf("read file err, excel:%v", file), err)
 	}
+	l.fileFs = f
 	defer func() {
+		l.fileFs = nil
 		if err = f.Close(); err != nil {
-			fmt.Printf("close file err, excel:%v", excelFile)
+			fmt.Printf("close file err, excel:%v", l.excelName)
 		}
 	}()
-
-	rows, err := f.GetRows(sheetName)
+	rows, err := f.GetRows(l.sheetName)
 	if err != nil {
-		return errors.Join(fmt.Errorf("get rows err, excel:%v", excelFile), err)
+		return errors.Join(fmt.Errorf("get rows err, excel:%v|shellName:%v", l.excelName, l.sheetName), err)
 	}
 
 	for rowIdx, row := range rows {
@@ -95,9 +107,9 @@ Next:
 	for i := range tt.NumField() {
 		field := tt.Field(i)
 		tag := field.Tag.Get("json")
-		for idx, name := range row {
+		for colIdx, name := range row {
 			if name == tag {
-				l.idxes[tag] = idx
+				l.idxes[tag] = colIdx
 				break Next
 			}
 		}
@@ -105,14 +117,96 @@ Next:
 	}
 }
 func (l *rowLoader[K, T]) parseDefault(row []string) {
-	l.defaultVals = make(map[string]string)
-	for name, idx := range l.idxes {
-		l.defaultVals[name] = row[idx]
+	l.defaultVals = make(map[int]string)
+	for _, idx := range l.idxes {
+		l.defaultVals[idx] = row[idx]
 	}
 }
 func (l *rowLoader[K, T]) parseLink(row []string) {
-	//先解析link?
+	l.link = make(map[int][][]string)
+	l.aliseName = make(map[int]map[string]string)
+	for _, idx := range l.idxes {
+		linkVal := row[idx]
+		if linkVal == "" {
+			continue
+		}
+		//
+		if subTables := strings.Split(linkVal, nestTag); len(subTables) == 2 { //parse nest table
+			val, err := l.readSubExcelSheet(subTables[0], subTables[1])
+			if err != nil {
+				panic(err)
+			}
+			l.link[idx] = val
+			//read other table and sheet
+		} else if nameAlise := strings.Split(linkVal, aliseTag); len(nameAlise) == 2 { //parse alise
+			rows, err := l.readSubExcelSheet(nameAlise[0], nameAlise[1])
+			if err != nil {
+				panic(err)
+			}
+			l.aliseName[idx] = make(map[string]string)
+			for i, aliseRow := range rows {
+				if i == 0 {
+					//第一行默认title
+					continue
+				}
+				//
+				l.aliseName[idx][aliseRow[0]] = nameAlise[1]
+			}
+		} else {
+			panic("link must \"|\" in table|sheet for sub table or \".\" in table.sheet for name alise")
+		}
+	}
 }
+func (l *rowLoader[K, T]) readSubExcelSheet(excelName, sheetName string) ([][]string, error) {
+	var subFileFs *excelize.File
+	//default excelName
+	if excelName == "" {
+		excelName = l.excelName
+	}
+
+	if excelName == l.excelName {
+		subFileFs = l.fileFs
+	} else {
+		f, err := excelize.OpenFile(l.excelName)
+		if err != nil {
+			return nil, errors.Join(fmt.Errorf("read sub excel/sheet err, excel:%v|sheet:%v", excelName, sheetName), err)
+		}
+		subFileFs = f
+	}
+	defer func() {
+		if excelName != l.excelName {
+			if err := subFileFs.Close(); err != nil {
+				fmt.Printf("close  sub excel/sheet err, excel:%v|sheet:%v", excelName, sheetName)
+			}
+		}
+	}()
+	rows, err := subFileFs.GetRows(l.sheetName)
+	if err != nil {
+		return nil, errors.Join(fmt.Errorf("get sub excel/sheet rows err, excel:%v|sheet:%v", excelName, sheetName), err)
+	}
+	return rows, nil
+}
+
 func (l *rowLoader[K, T]) parseData(row []string) {
 
+	for name, colIdx := range l.idxes {
+		val := row[colIdx]
+		//get val or default
+		if val == "" {
+			val = l.defaultVals[colIdx]
+		}
+
+		//check alise
+		if aliseMap, indexAliseOK := l.aliseName[colIdx]; indexAliseOK {
+			if aliseVal, aliseOk := aliseMap[val]; aliseOk {
+				val = aliseVal
+			}
+			continue
+		}
+
+		//todo get field type
+		//todo check link
+
+		//parse data
+	}
 }
