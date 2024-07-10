824b3f047887eaa96170539ef873d4bfdfb44497
lin <ichenzhl@qq.com>
2024-07-10 15:01:07 +0800

    开发ing add table [new] 设计表, 修改定义"_模板.xlsx" 2

--- apps/shared/table/1rowloader.go
+++ apps/shared/table/1rowloader.go
@@ -1,10 +1,12 @@
 package table
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/xuri/excelize/v2"
 	"reflect"
+	"strconv"
 	"strings"
 )
 
@@ -100,6 +102,14 @@ func (l *rowLoader[K, T]) Range(f func(k K, v T) bool) {
 }
 
 func (l *rowLoader[K, T]) parseHeader(row []string) {
+	//check
+	if len(row) == 0 {
+		panic(errors.New("row is empty"))
+	}
+	if row[0] != "id" {
+		panic(errors.New("row[0] must named id, now is " + row[0]))
+	}
+
 	l.idxes = make(map[string]int)
 	var t T
 	tt := reflect.TypeOf(t)
@@ -188,8 +198,13 @@ func (l *rowLoader[K, T]) readSubExcelSheet(excelName, sheetName string) ([][]st
 }
 
 func (l *rowLoader[K, T]) parseData(row []string) {
-
-	for name, colIdx := range l.idxes {
+	var t T
+	value := reflect.ValueOf(t)
+	for fieldName, colIdx := range l.idxes {
+		field := value.FieldByName(fieldName)
+		if !field.IsValid() {
+			panic(errors.New("field not exist, field fieldName:" + fieldName))
+		}
 		val := row[colIdx]
 		//get val or default
 		if val == "" {
@@ -203,10 +218,53 @@ func (l *rowLoader[K, T]) parseData(row []string) {
 			}
 			continue
 		}
-
-		//todo get field type
 		//todo check link
-
+		//todo map list format
 		//parse data
+		switch field.Kind() {
+		case reflect.String:
+			field.SetString(val)
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			var v int64
+			v, err := strconv.ParseInt(val, 0, field.Type().Bits())
+			if err != nil {
+				panic(errors.Join(fmt.Errorf("file:%s,sheet:%s,field:%s", l.excelName, l.sheetName, fieldName), err))
+			}
+			field.SetInt(v)
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			var v uint64
+			v, err := strconv.ParseUint(val, 0, field.Type().Bits())
+			if err != nil {
+				panic(errors.Join(fmt.Errorf("file:%s,sheet:%s,field:%s", l.excelName, l.sheetName, fieldName), err))
+			}
+			field.SetUint(v)
+		case reflect.Float32, reflect.Float64:
+			var v float64
+			v, err := strconv.ParseFloat(val, field.Type().Bits())
+			if err != nil {
+				panic(errors.Join(fmt.Errorf("file:%s,sheet:%s,field:%s", l.excelName, l.sheetName, fieldName), err))
+			}
+			field.SetFloat(v)
+		case reflect.Bool:
+			var v bool
+			v, err := strconv.ParseBool(val)
+			if err != nil {
+				panic(errors.Join(fmt.Errorf("file:%s,sheet:%s,field:%s", l.excelName, l.sheetName, fieldName), err))
+			}
+			field.SetBool(v)
+		case reflect.Ptr:
+			err := json.Unmarshal([]byte(val), field.Addr().Interface())
+			if err != nil {
+				panic(errors.Join(fmt.Errorf("file:%s,sheet:%s,field:%s", l.excelName, l.sheetName, fieldName), err))
+			}
+		case reflect.Struct:
+			panic(errors.Join(fmt.Errorf("file:%s,sheet:%s,field:%s", l.excelName, l.sheetName, fieldName), errors.New("must use pointer to struct")))
+		case reflect.Slice:
+			panic(errors.Join(fmt.Errorf("file:%s,sheet:%s,field:%s", l.excelName, l.sheetName, fieldName), errors.New("must replace with gsmodel.AList")))
+		case reflect.Map:
+			panic(errors.Join(fmt.Errorf("file:%s,sheet:%s,field:%s", l.excelName, l.sheetName, fieldName), errors.New("must replace with gsmodel.AMap")))
+		default:
+			panic(errors.Join(fmt.Errorf("file:%s,sheet:%s,field:%s", l.excelName, l.sheetName, fieldName), errors.New("unknown type")))
+		}
 	}
 }
